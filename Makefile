ed167acc273341c651b4e025ee577548c32ca57e
Shiyi Huo <shiyihuo@u-mandriva.csc.uvic.ca>
2017-11-19 17:32:07 -0800

    NVW: copy file not working

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ all: sfs
 sfs:
 	gcc -Wall diskinfo.c -o diskinfo
 	gcc -Wall disklist.c -o disklist
-	# gcc -Wall diskget.c -o diskget
+	gcc -Wall diskget.c -o diskget
 	# gcc -Wall diskput.c -o diskput
 
 .PHONY clean:
--- diskget.c
+++ diskget.c
@@ -15,41 +15,64 @@
 // 4.If (Last cluster of file && reach the filesize value), stop copy.
 
 
-char* concat_name_extension(char* file_name, char* file_extension) {
-  int i;
-  for (i = 0; i < 8; i++) {
-    if (p[i] == ' ') {
-      break;
-    }
-    file_name[i] = p[i];
-  }
-
-  for (i = 0; i < 3; i++) {
-    file_extension[i] = p[i+8];
-  }
-
-  strcat(file_name, ".");
-  strcat(file_name, file_extension);
-
-  return file_name;
-}
+// char* concat_name_extension(char* file_name, char* file_extension, char* p) {
+//   int i;
+//   for (i = 0; i < 8; i++) {
+//     if (p[i] == ' ') {
+//       break;
+//     }
+//     file_name[i] = p[i];
+//   }
+//
+//   for (i = 0; i < 3; i++) {
+//     file_extension[i] = p[i+8];
+//   }
+//
+//   char* result = malloc(strlen(file_name) + strlen(file_extension) + 1);
+//   strcpy(result, file_name);
+//   strcat(result, ".");
+//   strcat(result, file_extension);
+//
+//   return result;
+// }
 
 // return file size if file exists, otherwise return -1
-int get_file_size(char* file_name, char* p) {\
-  int file_size;
+int get_file_size(char* file_name, char* p) {
+  int file_size=0;
+
   while (p[0] != 0x00) {
     if ((p[11] & 0x02) == 0 && (p[11] & 0x08) == 0) { // not hidden, not volume label
-      char* curr_file_name = malloc(sizeof(char) * 8);
-      char* curr_file_extension = malloc(sizeof(char) * 3);
-      char* curr_file = malloc(strlen(curr_file_name) + strlen(curr_file_extension) + 1);
-      curr_file = concat_name_extension(curr_file_name, curr_file_extension);
+      char* curr_file_name = malloc(sizeof(char)*8);
+      char* curr_file_extension = malloc(sizeof(char)*3);
+      //char* curr_file = concat_name_extension(curr_file_name, curr_file_extension, p);
 
-      if (strcmp(file_name, curr_file) == 0) {
-        file_size = (p[28] & 0xFF) + ((p[29] & 0xFF) << 8) + ((p[30] & 0xFF) << 16) + ((p[31] & 0xFF) << 24);
+      int i;
+      for (i = 0; i < 8; i++) {
+        if (p[i] == ' ') {
+          break;
+        }
+        curr_file_name[i] = p[i];
+      }
+
+      for (i = 0; i < 3; i++) {
+        curr_file_extension[i] = p[i+8];
+      }
+
+      char* result = malloc(strlen(curr_file_name) + strlen(curr_file_extension) + 1);
+      strcpy(result, curr_file_name);
+      strcat(result, ".");
+      strcat(result, curr_file_extension);
+
+      printf("curr file: %s\n", result);
+      if (strcmp(file_name, result) == 0) {
+        file_size = ((p[28] & 0xFF) + ((p[29] & 0xFF) << 8) + ((p[30] & 0xFF) << 16) + ((p[31] & 0xFF) << 24));
+        printf("before return\n");
+        printf("file_size: %d\n", file_size);
         return file_size;
       }
     }
     p += 32;
+
   }
 
   return -1;
@@ -58,14 +81,33 @@ int get_file_size(char* file_name, char* p) {\
 // return first logical sector if it exists, otherwise return -1
 int get_first_logical_sector(char* file_name, char* p) {
   while (p[0] != 0x00) {
-    if ((p[11] & 0x02) == 0 && (p[11] & 0x08) == 0) { // not hidden, not volume label
-      char* curr_file_name = malloc(sizeof(char) * 8);
-      char* curr_file_extension = malloc(sizeof(char) * 3);
-      char* curr_file = malloc(strlen(curr_file_name) + strlen(curr_file_extension) + 1);
-      curr_file = concat_name_extension(curr_file_name, curr_file_extension);
+    if ((p[11] & 0x02) == 0 && (p[11] & 0x08) == 0) {
+      char* curr_file_name = malloc(sizeof(char)*8);
+      char* curr_file_extension = malloc(sizeof(char)*3);
+      //char* curr_file = malloc(strlen(curr_file_name) + strlen(curr_file_extension) + 1);
+      // char* curr_file = concat_name_extension(curr_file_name, curr_file_extension, p);
 
-      if (strcmp(file_name, curr_file) == 0) {
-        return p[26] + (p[27] << 8);
+      int i;
+      for (i = 0; i < 8; i++) {
+        if (p[i] == ' ') {
+          break;
+        }
+        curr_file_name[i] = p[i];
+      }
+
+      for (i = 0; i < 3; i++) {
+        curr_file_extension[i] = p[i+8];
+      }
+
+      char* result = malloc(strlen(curr_file_name) + strlen(curr_file_extension) + 1);
+      strcpy(result, curr_file_name);
+      strcat(result, ".");
+      strcat(result, curr_file_extension);
+
+      //printf("curr file: %s\n", result);
+
+      if (strcmp(file_name, result) == 0) {
+        return (p[26] + (p[27] << 8));
       }
     }
     p += 32;
@@ -90,13 +132,26 @@ int get_fat_entry(int entry, char* p) {
   return result;
 }
 
-//search uppercased filename from directory entries in root folder
-int search_file(char* file_name, char* p) {
+void copy_file(char* p, char* p2, char* file_name) {
+  int first_logical_sector = get_first_logical_sector(file_name, p + SECTOR_SIZE * 19);
+  int fat_entry = first_logical_sector;
+  int physical_entry;
+  printf("before file size\n");
+  int file_size = get_file_size(file_name, p + SECTOR_SIZE * 19);
+  printf("file size: %d", file_size);
+  int remaining_byte = file_size;
+  int i;
 
-}
-
-int copy_file(char* p, char* p2, char* file_name) {
-
+  do {
+    // read current byte
+    physical_entry = (33 + fat_entry - 2) * SECTOR_SIZE;  // byte in physical sector
+    for (i = 0; i < SECTOR_SIZE; i++) { // copy byte by byte
+      if (remaining_byte == 0)  break;
+      p2[file_size - remaining_byte] = p[i + physical_entry];
+      remaining_byte--;
+    }
+    fat_entry = get_fat_entry(fat_entry,p);
+  } while (fat_entry != 0xFFF);
 }
 
 
@@ -123,28 +178,36 @@ int main(int argc, char* argv[]) {
     exit(1);
   }
 
+  // search file in root directory
+  int file_size = get_file_size(argv[2], p + SECTOR_SIZE * 19);
+  // printf("main file_size: %d\n", file_size);
 
-  // int file_size = get_file_size(argv[2], p + SECTOR_SIZE * 19);
-  // if (file_size <= 0) {
-  //   printf("File not found.\n");
-  // } else {
-  //   // file to copy
-  //   int file2 = open(argv[2], O_RDWR | O_CREAT, 0666);
-  //   if (file2 == -1) {
-  //     printf("Error: failed to open file\n");
-  //     exit(1);
-  //   }
-  //   char* p2 = mmap(NULL, file_size, PROT_WRITE, MAP_SHARED, file2, 0);
-  //   if (p2 == MAP_FAILED) {
-  //     printf("Error: failed to map memory for file");
-  //     close(file);
-  //     exit(1);
-  //   }
+  if (file_size <= 0) {
+    printf("File not found.\n");
+  } else {
+    // create destination file
+    int file2 = open(argv[2], O_RDWR | O_CREAT, 0666);
+    if (file2 == -1) {
+      printf("Error: failed to open file\n");
+      exit(1);
+    }
 
-  //   copy_file(p,p2,argv[2]);
-  //   munmap(p2, file_size);
-  //   close(file2);
-  // }
+    // TODO:
+    // Seek to the last byte and write \0 to "stretch" the file
+		int result = lseek(file2, file_size-1, SEEK_SET);
+		result = write(file2, "", 1);
+
+    char* p2 = mmap(NULL, file_size, PROT_WRITE, MAP_SHARED, file2, 0);
+    if (p2 == MAP_FAILED) {
+      printf("Error: failed to map memory for file");
+      close(file);
+      exit(1);
+    }
+
+    copy_file(p,p2,argv[2]);  // copy from file to file2
+    munmap(p2, file_size);
+    close(file2);
+  }
 
   munmap(p, file_stat.st_size);
   close(file);
