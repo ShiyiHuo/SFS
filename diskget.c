370b417a0eea4309affad530cacfa4d991a03042
Shiyi Huo <shiyihuo@u-kubuntu.csc.uvic.ca>
2017-11-20 21:16:05 -0800

    fixed segmentation fault in diskget

--- diskget.c
+++ diskget.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
@@ -38,8 +39,6 @@
 
 // return file size if file exists, otherwise return -1
 int get_file_size(char* file_name, char* p) {
-  int file_size=0;
-
   while (p[0] != 0x00) {
     if ((p[11] & 0x02) == 0 && (p[11] & 0x08) == 0) { // not hidden, not volume label
       char* curr_file_name = malloc(sizeof(char)*20);
@@ -60,18 +59,14 @@ int get_file_size(char* file_name, char* p) {
       strcat(curr_file_name, ".");
       strcat(curr_file_name, curr_file_extension);
 
-      printf("curr file: %s\n", curr_file_name);
       if (strcmp(file_name, curr_file_name) == 0) {
-        file_size = ((p[28] & 0xFF) + ((p[29] & 0xFF) << 8) + ((p[30] & 0xFF) << 16) + ((p[31] & 0xFF) << 24));
+        int file_size = (p[28] & 0xFF) + ((p[29] & 0xFF) << 8) + ((p[30] & 0xFF) << 16) + ((p[31] & 0xFF) << 24);
         printf("before return\n");
-        printf("file_size: %d\n", file_size);
         return file_size;
       }
     }
     p += 32;
-
   }
-
   return -1;
 }
 
@@ -81,7 +76,7 @@ int get_first_logical_sector(char* file_name, char* p) {
     if ((p[11] & 0x02) == 0 && (p[11] & 0x08) == 0) {
       char* curr_file_name = malloc(sizeof(char)*20);
       char* curr_file_extension = malloc(sizeof(char)*3);
-      
+
       int i;
       for (i = 0; i < 8; i++) {
         if (p[i] == ' ') {
@@ -98,12 +93,11 @@ int get_first_logical_sector(char* file_name, char* p) {
       strcat(curr_file_name, curr_file_extension);
 
       if (strcmp(file_name, curr_file_name) == 0) {
-        return (p[26] + (p[27] << 8));
+        return (p[26]) + (p[27] << 8);
       }
     }
     p += 32;
   }
-
   return -1;
 }
 
@@ -113,11 +107,11 @@ int get_FAT_entry(int entry, char* p) {
   int result;
   if ((entry % 2) == 0) {
     a = p[SECTOR_SIZE + ((3 * entry) / 2) + 1] & 0x0F;  // low 4 bits
-    b = p[SECTOR_SIZE + ((3 * entry) / 2)];
+    b = p[SECTOR_SIZE + ((3 * entry) / 2)] & 0xFF;
     result = (a << 8) + b;
   } else {
     a = p[SECTOR_SIZE + (int)((3 * entry) / 2)] & 0xF0;  // high 4 bits
-    b = p[SECTOR_SIZE + (int)((3 * entry) / 2) + 1];
+    b = p[SECTOR_SIZE + (int)((3 * entry) / 2) + 1] & 0xFF;
     result = (a >> 4) + (b << 4);
   }
   return result;
@@ -126,16 +120,14 @@ int get_FAT_entry(int entry, char* p) {
 void copy_file(char* p, char* p2, char* file_name) {
   int first_logical_sector = get_first_logical_sector(file_name, p + SECTOR_SIZE * 19);
   int fat_entry = first_logical_sector;
-  int physical_entry;
-  printf("before file size\n");
+  int physical_entry = (33 + fat_entry - 2) * SECTOR_SIZE;
   int file_size = get_file_size(file_name, p + SECTOR_SIZE * 19);
-  printf("file size: %d", file_size);
   int remaining_byte = file_size;
-  int i;
 
   do {
-    // read current byte
+    //read current byte
     physical_entry = (33 + fat_entry - 2) * SECTOR_SIZE;  // byte in physical sector
+    int i;
     for (i = 0; i < SECTOR_SIZE; i++) { // copy byte by byte
       if (remaining_byte == 0)  break;
       p2[file_size - remaining_byte] = p[i + physical_entry];
@@ -183,10 +175,31 @@ int main(int argc, char* argv[]) {
       exit(1);
     }
 
+
+
+
     // TODO:
     // Seek to the last byte and write \0 to "stretch" the file
+    // Seek to the last byte and write \0 to "stretch" the file
 		int result = lseek(file2, file_size-1, SEEK_SET);
+		if (result == -1) {
+			munmap(p, file_stat.st_size);
+			close(file);
+			close(file2);
+			printf("Error: failed to seek to end of file\n");
+			exit(1);
+		}
 		result = write(file2, "", 1);
+		if (result != 1) {
+			munmap(p, file_stat.st_size);
+			close(file);
+			close(file2);
+			printf("Error: failed to write last byte\n");
+			exit(1);
+    }
+
+
+
 
     char* p2 = mmap(NULL, file_size, PROT_WRITE, MAP_SHARED, file2, 0);
     if (p2 == MAP_FAILED) {
